96360c061c1004fe16a8ddd0d58f2236132ded66
Dian Yuanda <Dian.Yuanda@xapiens.id>
2025-11-03 15:57:53 +0700

    fix: build

--- app/protected/layout.tsx
+++ app/protected/layout.tsx
@@ -1,15 +1,9 @@
-import Link from "next/link";
 import { NextIntlClientProvider } from "next-intl";
 
-import { hasEnvVars } from "@/lib/utils";
+import HomeScreen from "@/modules/app/home-screen";
 
 import { Toaster } from "@/components/atoms/toaster";
-import { AuthButton } from "@/components/auth-button";
-import { DeployButton } from "@/components/deploy-button";
-import { EnvVarWarning } from "@/components/env-var-warning";
-import { ThemeSwitcher } from "@/components/organisms/theme-switcher";
 import { TooltipProvider } from "@/components/atoms/tooltip";
-import HomeScreen from "@/modules/app/home-screen";
 
 export default function ProtectedLayout({
   children,
